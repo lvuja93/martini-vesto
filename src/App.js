cbca26abaab9767ddc7aa76f1265280e28f702a9
Lazar <lazar.vujovic1993@gmail.com>
2024-11-25 19:05:38 +0100

    laki pravi sajt

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Laki pravi sajt motherfuckers
         </a>
       </header>
     </div>
