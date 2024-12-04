470a8cd14c819be482ccca7b085432d1793be2fa
Lazar <lazar.vujovic1993@gmail.com>
2024-12-04 17:43:43 +0100

    kontakt

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -10,33 +10,40 @@ const Kontakt = () => {
                     <h2 className="text-3xl font-bold pb-8 text-center">Postavite nam pitanje</h2>
                     <EmailForm1 />
                 </div>
-                <div className="w-full lg:w-1/2 py-24 flex flex-col justify-center items-start md:pl-36 font-medium space-y-8 rounded-xl text-xl">
+                <div class="w-full lg:w-1/2 py-24 flex flex-col justify-center items-start md:pl-36 font-medium space-y-8 rounded-xl text-xl">
 
-                    <div className='flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer'>
-                    <a href="https://www.instagram.com/martinivestoofficial/" target="_blank" rel="noopener noreferrer" className="hover:text-[#f5821f] transition duration-300 cursor-pointer">
-                        <FaInstagram className='w-8 h-8' />
-                        <p>@martinivestoofficial</p>
-                    </a>    
+                    <div class="flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer">
+                        <a href="https://www.instagram.com/martinivestoofficial/" target="_blank" rel="noopener noreferrer" class="flex items-center space-x-4">
+                            <FaInstagram className="w-8 h-8" />
+                            <p class="ml-4">@martinivestoofficial</p>
+                        </a>
                     </div>
-                    <div className='flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer'>
-                    <a href="https://www.facebook.com/martinivesto.gr" target="_blank" rel="noopener noreferrer" className="hover:text-[#f5821f] transition duration-300 cursor-pointer">
-                        <FaFacebook className='w-8 h-8' />
-                        <p>Martini Vesto</p>
-                    </a>    
+
+
+                    <div class="flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer">
+                        <a href="https://www.facebook.com/martinivesto.gr" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-4">
+                            <FaFacebook class="w-8 h-8" />
+                            <p class="ml-4">Martini Vesto</p>
+                        </a>
                     </div>
-                    <div className='flex items-center space-x-4 pl-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer'>
-                    <a href="mailto:info@martini-vesto.com" className="hover:text-[#f5821f] transition duration-300 cursor-pointer">
-                        <FaEnvelope className='w-8 h-8' />
-                        <p>info@martini-vesto.com</p>
-                    </a>
+
+
+                    <div class="flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer">
+                        <a href="mailto:info@martini-vesto.com" className="flex items-center space-x-4">
+                            <FaEnvelope class="w-8 h-8" />
+                            <p class="ml-4">info@martini-vesto.com</p>
+                        </a>
                     </div>
-                    <div className='flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer'>
-                        <a href="tel:+38116215827" className='flex items-center space-x-4'>
-                            <FaPhoneAlt className='w-8 h-8' />
-                            <p>+381 16 215827</p>
+
+
+                    <div class="flex items-center space-x-4 p-1 rounded-md hover:text-[#f5821f] transition duration-300 cursor-pointer">
+                        <a href="tel:+38116215827" class="flex items-center space-x-4">
+                            <FaPhoneAlt class="w-8 h-8" />
+                            <p class="ml-4">+381 16 215827</p>
                         </a>
                     </div>
                 </div>
+
             </div>
         </div>
     );
